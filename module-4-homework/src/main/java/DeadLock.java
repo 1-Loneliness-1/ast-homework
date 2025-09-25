e59eebdc78014a83349564eee3f25bdbc97b5df7
Kisurin Konstantin <costya.kisurin@yandex.ru>
2025-09-25 17:40:49 +0300

    fix(module_4): класс, моделирующий DeadLock, был переписан во избежание дублирования кода

--- module-4-homework/src/main/java/DeadLock.java
+++ module-4-homework/src/main/java/DeadLock.java
@@ -5,33 +5,36 @@ public class DeadLock {
     private static final ReentrantLock lock1 = new ReentrantLock(true);
     private static final ReentrantLock lock2 = new ReentrantLock(true);
 
-    public static void startDeadLockSituation() {
-        Thread thread1 = new Thread(() -> {
+    private static class Worker implements Runnable {
+
+        private final String name;
+        private final ReentrantLock lock1;
+        private final ReentrantLock lock2;
+
+        Worker (String name, ReentrantLock lock1, ReentrantLock lock2) {
+            this.name = name;
+            this.lock1 = lock1;
+            this.lock2 = lock2;
+        }
+
+        @Override
+        public void run() {
             lock1.lock();
-            System.out.println("Монитор 1 захвачен. Пробую захватить второй...");
+            System.out.printf("Захват одного из ресурсов потоком %s. Пробую захватить следующий...\n", name);
             try {
                 lock2.lock();
-                System.out.println("Монитор 2 захвачен.");
+                System.out.printf("Второй ресурс захвачен потоком %s.\n", name);
             } finally {
                 lock1.unlock();
                 lock2.unlock();
+                System.out.printf("Все ресурсы освобождены, поток %s заканчивает работу.\n", name);
             }
+        }
+    }
 
-            lock2.lock();
-        });
-        Thread thread2 = new Thread(() -> {
-            lock2.lock();
-            System.out.println("Монитор 2 захвачен. Пробую захватить первый...");
-            try {
-                lock1.lock();
-                System.out.println("Монитор 1 захвачен.");
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
-        });
-        thread1.start();
-        thread2.start();
+    public static void startDeadLockSituation() {
+        new Thread (new Worker ("Поток #1", lock1, lock2)).start();
+        new Thread (new Worker ("Поток #2", lock2, lock1)).start();
     }
 
 }
